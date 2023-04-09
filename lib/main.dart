38956cba6a9c45ac1301032e7823e05babbecff0
Nick J <nickj7010@gmail.com>
2023-04-09 18:02:32 -0400

    add plugins for auth support and code for later

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@
 
 import 'dart:math';
 import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 WebSocketChannel ws =
@@ -815,6 +818,8 @@ class SearchPageState extends State<SearchPage>
 class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Clipboard.setData(const ClipboardData(text: 'abcdefg'));
+    //launchUrl(Uri.https('www.google.com', '/device'));
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 15, 15, 15),
